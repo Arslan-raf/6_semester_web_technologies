de24ba8aa1ff1228ef1711228242c2fe9d3bea67
Arslan-raf <61138541+Arslan-raf@users.noreply.github.com>
2021-05-25 17:25:26 +0300

    ЛР3
    лабораторная работа №3

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<p> Лабораторные работы за 5 семестр </p>
+<li><a href="Yarsev/index.php">Теоретические основы экономических информационных систем</a></li>
+	<li><a href="www/lab1.php">Лабораторная работа 1 </a></li>
+	<li><a href="www/LabaPHP2.php">Лабораторная работа 2 </a></li>
+	<li><a href="laba3/LabaPHP3.php">Лабораторная работа 3 </a></li>
+
+	<li><a href="bd_user/index2.php">Лабораторная работа 4 </a></li>
+
+<li><a href="bd5/index2.php">Лабораторная работа 5 </a></li>
+
+
+	<H1>Лабораторные работы за 6 семестр.</H1>
+<li><a href="Lb1/laba1main.html">Лабораторная работа 1 </a></li>
+<li><a href="Lb2/lab2main.html">Лабораторная работа 2 </a></li>
+<li><a href="lab3/index.html">Лабораторная работа 3 </a></li>
---
+++ lab3/src/api/connect.php
@@ -0,0 +1,8 @@
+<?php
+$host = 'localhost';
+$user = 'f0473065_root';
+$password = 'root';
+$database = 'f0473065_DB';
+$queryTab0 = 'news';
+$queryTab1 = 'comments';
+?>
